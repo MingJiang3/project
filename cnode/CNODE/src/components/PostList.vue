85d6f43a9aca2104cf5e47fadee612fd0d39755f
MingJiang3 <457918455@qq.com>
2018-09-25 22:21:34 +0800

    add article

--- cnode/CNODE/src/components/PostList.vue
+++ cnode/CNODE/src/components/PostList.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="loading" v-if="isLoading">
-      <img src="" alt="">               <!-- 加载动画，还未选取 -->
+      <img src="../assets/loading.gif" width="40px" height="40px">               <!-- 加载动画 -->
     </div>
         <!-- 帖子列表 -->
-    <div class="posts">
+    <div class="posts" v-else>
       <ul>
         <li>
           <div class="toobar">
@@ -17,9 +17,9 @@
         </li>
         <li v-for="post in posts">
                 <!-- 动态绑定获取头像 -->
-          <img :src="post.author.avatar_url" alt="">
+          <img :src="post.author.avatar_url" class="headPortrait">
                 <!-- 回复/浏览数 -->
-          <span>
+          <span class="visit_count">
             <span class="reply_count">{{post.reply_count}}</span>/{{post.visit_count}}
           </span>
                 <!-- 帖子分类 -->
@@ -27,7 +27,9 @@
                 {{post | tabFormatter}}
           </span>
                 <!-- 获取标题 -->
-          <span>{{post.title}}</span>
+                <router-link :to="{name:'post_content',params:{id:post.id}}">
+                  <span>{{post.title}}</span>
+                </router-link>          
                 <!-- 最终回复时间 -->
           <span class="last_reply">{{post.last_reply_at | formatDate}}</span>
         </li>
@@ -37,40 +39,149 @@
 </template>
 
 <script>
-  export default {
-    name: "PostList",
-    data(){
-      return{
-        isLoading: false ,
-        posts:[]                             //页面的列表数组
-      }
-    },
-    methods:{
-      getData(){
-        this.$http.get('https://cnodejs.org/api/v1/topics',{
-          page:1,
-          limit:20
+export default {
+  name: "PostList",
+  data() {
+    return {
+      isLoading: false,
+      posts: [] //页面的列表数组
+    };
+  },
+  methods: {
+    getData() {
+      this.$http
+        .get("https://cnodejs.org/api/v1/topics", {
+          page: 1,
+          limit: 20
         })
-          .then(res=>{
-            this.isLoading = false      //加载成功去除动画
-             console.log(res)
-          })
-          .catch(function(err){
-            console.log(err)
-          })
-
-      }
-    },
-      beforeMount(){
-        this.isLoading = true       //加载成功之前显示加载动画
-        this.getData()                  //在页面加载之前获取数据
-      }
+        .then(res => {
+          this.isLoading = false; //加载成功去除动画
+          this.posts = res.data.data;
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
+  },
+  beforeMount() {
+    this.isLoading = true; //加载成功之前显示加载动画
+    this.getData(); //在页面加载之前获取数据
   }
+};
 </script>
 
 <style scoped>
-    img{
-        width: 30px;
-        height: 30px;
-    }
+.headPortrait {
+  width: 30px;
+  height: 30px;
+}
+.PostList {
+  background-color: #e1e1e1;
+}
+.posts {
+  margin-top: 10px;
+}
+.PostList img {
+  height: 30px;
+  width: 30px;
+  vertical-align: middle;
+}
+ul {
+  list-style: none;
+  width: 100%;
+  max-width: 1344px;
+  margin: 0 auto;
+}
+ul li:not(:first-child) {
+  padding: 9px;
+  font-size: 15px;
+  font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma,
+    "Hiragino Sans GB", STHeiti, sans-serif !important;
+  font-weight: 400;
+  background-color: white;
+  color: #333;
+  border-top: 1px solid #f0f0f0;
+}
+li:not(:first-child):hover {
+  background: #f5f5f5;
+}
+li:last-child:hover {
+  background: white;
+}
+li span {
+  line-height: 30px;
+}
+.allcount {
+  width: 70px;
+  display: inline-block;
+  text-align: center;
+  font-size: 12px;
+}
+.visit_count {
+  color: #b4b4b4;
+  font-size: 14px;
+  margin: 0px 5px;
+}
+.reply_count {
+  color: #9e78c0;
+  font-size: 14px;
+}
+.put_good,
+.put_top {
+  background: #80bd01;
+  padding: 2px 4px;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  color: #fff;
+  font-size: 12px;
+  margin-right: 10px;
+}
+.topiclist-tab {
+  background-color: #e5e5e5;
+  color: #999;
+  padding: 2px 4px;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  font-size: 12px;
+  margin-right: 10px;
+}
+.last_reply {
+  text-align: right;
+  min-width: 50px;
+  display: inline-block;
+  white-space: nowrap;
+  float: right;
+  color: #778087;
+  font-size: 12px;
+}
+.toobar {
+  height: 40px;
+  background-color: #f5f5f5;
+}
+.toobar span {
+  font-size: 14px;
+  color: #80bd01;
+  line-height: 40px;
+  margin: 0 10px;
+  cursor: pointer;
+}
+.toobar span:hover {
+  color: #9e78c0;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+a:hover {
+  text-decoration: underline;
+}
+.loading {
+  text-align: center;
+  padding-top: 100px;
+}
 </style>
