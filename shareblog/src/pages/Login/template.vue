bc75a0f0e3b8ac6f1f9644a5b6e1d540daadf450
MingJiang3 <457918455@qq.com>
2018-09-29 12:21:04 +0800

    init  blog  frame

---
+++ shareblog/src/pages/Login/template.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="login">
+        登录
+    </div>
+</template>
+<script src="./template.js"></script>
+<style src="./template.css"></style>
---
+++ shareblog/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '../pages/index/template'
+import My from '../pages/My/template'
+import Edit from '../pages/Edit/template'
+import Create from '../pages/Create/template'
+import Detail from '../pages/Detail/template'
+import Login from '../pages/Login/template'
+import User from '../pages/User/template'
+import Register from '../pages/Regidter/template'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Index
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/my',
+      component: My
+    },
+    {
+      path: '/uer',
+      component: User
+    },
+    {
+      path: '/edit',
+      component: Edit
+    },
+    {
+      path: '/create',
+      component: Create
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/detail',
+      component: Detail
+    }
+  ]
+})
