18406bf8bcf3b76a42850185fcaa0917193be783
MingJiang3 <457918455@qq.com>
2018-10-26 21:13:53 +0800

    添加商品两种状态下的选择

--- re_youzan/src/pages/cart/cart.js
+++ re_youzan/src/pages/cart/cart.js
@@ -34,6 +34,23 @@ new Vue({
                 })
             }
         },
+        allRemoveSelected:{
+            get(){
+                if(this.editingShop){
+                    return this.editingShop.removeChecked
+                }
+                return false
+            },
+            set(newVal){
+                if(this.editingShop){
+                    this.editingShop.removeChecked = newVal
+                    this.editingShop.goodsList.forEach(good=>{
+                        good.removeChecked = newVal
+                    })
+                }
+                return false
+            }
+        },
         selectLists(){
             let arr = []
             let total = 0
@@ -52,7 +69,16 @@ new Vue({
             return []
         },
         removeLists(){
-
+            if(this.editingShop){
+                let arr = []
+                this.editingShop.goodsList.forEach(good=>{
+                    if(good.removeChecked){
+                        arr.push(good)
+                    }
+                })
+                return arr
+            }
+            return []
         }
     },
     created(){
@@ -77,19 +103,22 @@ new Vue({
             })
         },
         selectGood(shop,good){
-            good.checked = !good.checked
-            shop.checked = shop.goodsList.every(good=>{ //一个店铺中的商品全选时，店铺就选中
-                return good.checked
+            let attr = this.editingShop ? 'removeChecked':'checked'
+            good[attr] = !good[attr]
+            shop[attr] = shop.goodsList.every(good=>{ //一个店铺中的商品全选时，店铺就选中
+                return good[attr]
             })
         },
         selectShop(shop){
-            shop.checked = !shop.checked
+            let attr = this.editingShop ? 'removeChecked':'checked'
+            shop[attr] = !shop[attr]
             shop.goodsList.forEach(good=>{  //店铺选中商品就全选
-                good.checked = shop.checked
+                good[attr] = shop[attr]
             })
         },
         selectAll(){
-            this.allSelected = !this.allSelected
+            let attr = this.editingShop ? 'allRemoveSelected':'allSelected'
+            this[attr] = !this[attr]
         },
         edit(shop,shopIndex){
             shop.editing = !shop.editing
