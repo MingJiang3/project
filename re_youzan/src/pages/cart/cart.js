917281fb104fe0d6c9175deacf99b8277ab8ad07
MingJiang3 <457918455@qq.com>
2018-10-26 11:25:34 +0800

    添加购物车商品请求

--- re_youzan/src/pages/cart/cart.js
+++ re_youzan/src/pages/cart/cart.js
@@ -2,3 +2,29 @@ import './cart_base.css'
 import './cart_trade.css'
 import './cart.css'
 
+import Vue from 'vue'
+import mixin from 'js/mixin.js';
+import axios from 'axios'
+import url from 'js/api.js'
+
+new Vue({
+    el:'.container',
+    data:{
+        lists:null,
+
+    },
+    computed:{
+
+    },
+    created(){
+        this.getLists()
+    },
+    methods:{
+        getLists(){
+            axios.post(url.carLists).then(res=>{
+                this.lists = res.data.cartList
+            })
+        }
+    },
+    mixins:[mixin]
+})
