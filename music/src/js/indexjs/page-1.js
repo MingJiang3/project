0ab6d0a2a1d58a19b7045c6acf0f4c419b049cf6
MingJiang3 <457918455@qq.com>
2018-11-04 13:01:54 +0800

    url失效修復

--- music/src/js/indexjs/page-1.js
+++ music/src/js/indexjs/page-1.js
@@ -34,7 +34,6 @@
             let script1 = document.createElement('script')
             script1.src = './js/indexjs/page-1-1.js'
             script1.onload = function(){
-                console.log('加载1')
             }
             document.body.appendChild(script1)
         },
@@ -42,7 +41,6 @@
             let script2 = document.createElement('script')
             script2.src = './js/indexjs/page-1-2.js'
             script2.onload = function(){
-                console.log('加载2')
             }
             document.body.appendChild(script2)
         }
