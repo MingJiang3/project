ab158832b6e519afc6a04c5c0a45b78846a4ab93
MingJiang3 <457918455@qq.com>
2019-07-24 11:59:28 +0800

    change to easyMock

--- re_youzan/src/modules/js/api.js
+++ re_youzan/src/modules/js/api.js
@@ -1,27 +1,28 @@
+// let host = 'http://rap2api.taobao.org/app/mock/7058'
 let url = {
-    hostLists:'/index/hotLists',
-    banner:'/index/banner',
-    topList:'/category/topList',
-    rank:'/category/rank',
-    subList:'/category/subList',
-    searchList:'/search/list',
-    details:'/goods/details',
-    deal:'/goods/deal',
-    addCart:'/cart/add',
-    carLists:'/cart/list',
-    cartReduce:'/cart/reduce',
-    cartRemove:'/cart/remove',
-    cartMrremove:'/cart/mrremove',
-    cartUpdate:'/cart/update',
-    addressLists:'/address/list',
-    addressAdd:'/address/add',
-    addressRemove:'/address/remove',
-    addressUpdate:'/address/update',
-    addressSetDefault:'/address/setDefault',
+    hostLists: '/index/hotLists',
+    banner: '/index/banner',
+    topList: '/category/topList',
+    rank: '/category/rank',
+    subList: '/category/subList',
+    searchList: '/search/list',
+    details: '/goods/details',
+    deal: '/goods/deal',
+    addCart: '/cart/add',
+    carLists: '/cart/list',
+    cartReduce: '/cart/reduce',
+    cartRemove: '/cart/remove',
+    cartMrremove: '/cart/mrremove',
+    cartUpdate: '/cart/update',
+    addressLists: '/address/list',
+    addressAdd: '/address/add',
+    addressRemove: '/address/remove',
+    addressUpdate: '/address/update',
+    addressSetDefault: '/address/setDefault',
 }
-let host = 'http://rap2api.taobao.org/app/mock/7058'
-for(const key in url){
-    if(url.hasOwnProperty(key)){
+let host = 'https://www.easy-mock.com/mock/5d1c664acdd7f73b2f454fe1'
+for (const key in url) {
+    if (url.hasOwnProperty(key)) {
         url[key] = host + url[key];
     }
 }
